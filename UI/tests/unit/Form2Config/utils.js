9d0a9987b7e8ccc0b12b4c634d75f23c45d4a4a6
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-05-16 15:49:41 +0200

    Test config when father has been added with specific values.

--- UI/tests/unit/Form2Config/utils.js
+++ UI/tests/unit/Form2Config/utils.js
@@ -75,3 +75,60 @@ limit.pm.to.P=T
 value.of.P=0.15
 self.contained=T
 regions.flanking.size=1000000`;
+
+
+async function inputSampleID(wrapper, value){
+    await wrapper.find('#input_sample_id').setValue(value);
+};
+async function inputGender(wrapper,value){
+    await wrapper.findComponent({name:"InputGender"}).vm.$emit('input',value);
+};
+async function inputInformativeID(wrapper,boolean){
+    await wrapper.find('#input_informative_id').setChecked(boolean);
+};
+async function inputAffected(wrapper,value){
+    await wrapper.findComponent({name:"InputAffected"}).vm.$emit('input',value);
+};
+async function inputDpHardLimitID(wrapper,boolean){
+    await wrapper.find('#input_dp_hard_limit_id').setChecked(boolean);
+};
+async function inputAfHardLimitID(wrapper,boolean){
+    await wrapper.find('#input_af_hard_limit_id').setChecked(boolean);
+};
+
+class FatherInputs {
+    constructor(params){
+        this.sampleID = "defaultFatherID";
+        this.gender = "M";
+        this.keepInformativeIDs = true;
+        this.diseaseStatus =  "NA";
+        this.keepLimitIDHardDP =  true;
+        this.keepLimitIDHardAF = true;
+        for (let key in params) {
+            this[key] = params[key];
+        };
+    };
+}
+export async function addFather(
+        wrapper,
+        params={},
+){
+    let Inputs = new FatherInputs(params);
+    await wrapper.find('#tab_pedigree').trigger('click');
+    await wrapper.vm.$nextTick();
+    await wrapper.find('#input_father_add').trigger('click');
+    await wrapper.vm.$nextTick();
+
+    // select father card element
+    let fatherCardElm = await wrapper.find('#input_father_card');
+
+    // fill in fields
+    await inputSampleID(fatherCardElm, Inputs.sampleID);
+    await inputGender(fatherCardElm, Inputs.gender);
+    await inputInformativeID(fatherCardElm,Inputs.keepInformativeIDs);
+    await inputAffected(fatherCardElm, Inputs.diseaseStatus);
+    await inputDpHardLimitID(fatherCardElm, Inputs.keepLimitIDHardDP);
+    await inputAfHardLimitID(fatherCardElm, Inputs.keepLimitIDHardAF);
+    
+    await wrapper.vm.$nextTick(); 
+}
